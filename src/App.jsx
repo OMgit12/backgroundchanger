8262ebae6d8ea456e3a507c18c55376f7669697b
hariom <hariomsh.jpr@gmail.com>
2024-08-07 17:11:04 +0530

    add new doubleclick feature

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center shadow-lg bg-white px-3 py-3 rounded-3xl">
           <button
-            onClick={() => setcolor("red")}
+            onClick={() => setcolor("red")} onDoubleClick={()=> setcolor("olive")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "red" }}
           >
